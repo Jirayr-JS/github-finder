4702ef72d51cbb258baad6c1fbf2c5cbb552d301
JIRAYR <69183073+JIRAYR550@users.noreply.github.com>
2022-01-30 10:42:19 +0400

    initial

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Alert from './components/layout/Alert';
+import Home from './pages/Home';
+import About from './pages/About';
+import User from './pages/User';
+import NotFound from './pages/NotFound';
+import { GithubProvider } from './context/github/GithubContext';
+import { AlertProvider } from './context/alert/AlertContext';
+
+function App() {
+  return (
+    <GithubProvider>
+      <AlertProvider>
+        <Router>
+          <div className="flex flex-col justify-between h-screen">
+            <Navbar />
+
+            <main className="container mx-auto px-3 pb-12">
+              <Alert />
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/about" component={About} />
+                <Route path="/user/:login" component={User} />
+                <Route path="/notfound" component={NotFound} />
+                <Route component={NotFound} />
+              </Switch>
+            </main>
+
+            <Footer />
+          </div>
+        </Router>
+      </AlertProvider>
+    </GithubProvider>
+  );
+}
+
+export default App;
